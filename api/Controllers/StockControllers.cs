1a3d4b1e7411f1b639fbb74b4c621e1faba62ca7
vzxtq <fixitex0206@gmail.com>
2024-12-17 22:59:11 +0100

    After Delete Commit

---
+++ api/Controllers/StockControllers.cs
@@ -0,0 +1,64 @@
+using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockControllers : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockControllers(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll(){
+            var stocks = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+{
+    var stockModel = stockDto.ToStockFromCreateDto();
+    _context.Stocks.Add(stockModel);
+    _context.SaveChanges();
+
+    // Здесь создаем объект DTO без Id для ответа
+    var responseDto = new StockDto
+    {
+        Symbol = stockModel.Symbol,
+        CompanyName = stockModel.CompanyName,
+        Purchase = stockModel.Purchase,
+        LastDiv = stockModel.LastDiv,
+        Industry = stockModel.Industry,
+        MarketCap = stockModel.MarketCap
+    };
+
+    // Возвращаем новый ответ без Id
+    return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, responseDto);
+}
+
+    }
+}
